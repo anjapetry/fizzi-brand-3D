f8695b4e1258a2ea9734e1302a4ef984063fe75a
Anja Petry <anjapetry@protonmail.com>
2024-10-09 20:07:20 +0200

    modify Canvas styles in src/components/ViewCanvas.tsx

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -2,9 +2,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
-
-import FloatingCan from "@/components/FloatingCan";
+import { View } from "@react-three/drei";
 
 type Props = {};
 
@@ -27,8 +25,7 @@ export default function ViewCanvas({}: Props) {
         fov: 30,
       }}
     >
-      <FloatingCan />
-      <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
+      <View.Port />
     </Canvas>
   );
 }
