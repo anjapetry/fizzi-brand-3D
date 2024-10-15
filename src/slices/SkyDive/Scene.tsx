72f797e1c1045b4711e3bd01425df10a4562835f
Anja Petry <anjapetry@protonmail.com>
2024-10-15 21:00:09 +0200

    feat: implement drei's text element in src/slices/SkyDive/Scene.tsx

--- src/slices/SkyDive/Scene.tsx
+++ src/slices/SkyDive/Scene.tsx
@@ -4,8 +4,15 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 import { Content } from "@prismicio/client";
-import { Cloud, Clouds, Environment, OrbitControls } from "@react-three/drei";
+import {
+  Cloud,
+  Clouds,
+  Environment,
+  OrbitControls,
+  Text,
+} from "@react-three/drei";
 import FloatingCan from "@/components/FloatingCan";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 type SkyDiveProps = {
   sentence: string | null;
@@ -20,6 +27,16 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
   const cloudsRef = useRef<THREE.Group>(null);
   const wordsRef = useRef<THREE.Group>(null);
 
+  const ANGLE = 75 * (Math.PI / 180);
+
+  const getXPosition = (distance: number) => distance * Math.cos(ANGLE);
+  const getYPosition = (distance: number) => distance * Math.sin(ANGLE);
+
+  const getXYPositions = (distance: number) => ({
+    x: getXPosition(distance),
+    y: getYPosition(-1 * distance),
+  });
+
   return (
     <group ref={groupRef}>
       <group rotation={[0, 0, 0.5]}>
@@ -32,7 +49,10 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
         <Cloud ref={cloud2Ref} bounds={[10, 10, 2]} />
       </Clouds>
 
-      {/* Words */}
+      {/* Text */}
+      <group ref={wordsRef}>
+        {sentence && <ThreeText sentence={sentence} color="#F97355" />}
+      </group>
 
       <OrbitControls />
 
@@ -41,4 +61,33 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
       <Environment files="/hdr/field.hdr" environmentIntensity={1.5} />
     </group>
   );
+}
+
+function ThreeText({
+  sentence,
+  color = "blue",
+}: {
+  sentence: string;
+  color?: string;
+}) {
+  const words = sentence.toUpperCase().split(" ");
+
+  const material = new THREE.MeshLambertMaterial();
+  const isDesktop = useMediaQuery("(min-width: 950px)", true);
+
+  return words.map((word: string, wordIndex: number) => (
+    <Text
+      key={`${wordIndex}-${word}`}
+      scale={isDesktop ? 1 : 0.5}
+      color={color}
+      material={material}
+      font="/fonts/Alpino-Variable.woff"
+      fontWeight={900}
+      anchorX={"center"}
+      anchorY={"middle"}
+      characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ!,.?'"
+    >
+      {word}
+    </Text>
+  ));
 }
